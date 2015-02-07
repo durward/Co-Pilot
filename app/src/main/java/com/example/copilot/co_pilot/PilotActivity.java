52a7cde35de5188d4c130bd37c6f6183869c7397
Jacqueline M Ali <dragonperson@gmail.com>
2015-02-07 15:19:38 -0500

    Adding to onCreate

--- app/src/main/java/com/example/copilot/co_pilot/PilotActivity.java
+++ app/src/main/java/com/example/copilot/co_pilot/PilotActivity.java
@@ -2,22 +2,33 @@ package com.example.copilot.co_pilot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 /**
- * Created by durwardbenham3 on 2/7/15.
+ * Created by durwardbenham3 and jacquelineali on 2/7/15.
  */
 public class PilotActivity extends Activity{
     Button send;
     EditText message;
     EditText number;
 
+    //
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pilot);
 
         send = (Button) findViewById(R.id.send);
+        message = (EditText) findViewById(R.id.message);
+        number = (EditText) findViewById(R.id.number);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ADD 2:28
+            }
+        });
+
 
     }
 }
