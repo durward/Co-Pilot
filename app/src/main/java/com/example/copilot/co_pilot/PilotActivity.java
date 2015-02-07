20f049aa2e610242cc6a199dd76f8521ad062e34
Jacqueline M Ali <dragonperson@gmail.com>
2015-02-07 15:55:44 -0500

    Adding Top and Bottom Vars

--- app/src/main/java/com/example/copilot/co_pilot/PilotActivity.java
+++ app/src/main/java/com/example/copilot/co_pilot/PilotActivity.java
@@ -2,6 +2,7 @@ package com.example.copilot.co_pilot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,25 +11,49 @@ import android.widget.EditText;
  * Created by durwardbenham3 and jacquelineali on 2/7/15.
  */
 public class PilotActivity extends Activity{
-    Button sendBtn;
-    EditText message;
-    EditText number;
+    // For sending to co-pilots
+    Button grpSendBtn;
+    EditText grpMsg;
+    EditText grpNums;
 
-    //
+    // For sending to innocent bystander
+    Button passengerSendBtn;
+    EditText passengerMsg;
+    EditText passengerNum;
+
+
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pilot);
 
-        sendBtn = (Button) findViewById(R.id.sendButton);
-        message = (EditText) findViewById(R.id.message);
-        number = (EditText) findViewById(R.id.number);
-        sendBtn.setOnClickListener(new View.OnClickListener() {
+        grpSendBtn = (Button) findViewById(R.id.sendButtonTop);
+        grpMsg = (EditText) findViewById(R.id.messageTop);
+        grpNums = (EditText) findViewById(R.id.numberTop);
+
+        passengerSendBtn = (Button) findViewById(R.id.sendButtonBottom);
+        passengerMsg = (EditText) findViewById(R.id.messageBottom);
+        passengerNum = (EditText) findViewById(R.id.numberBottom);
+
+
+        // When clicked do this:
+        passengerSendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // ADD 2:28
+                String myMsg = passengerMsg.getText().toString();
+                String phoneNumber = passengerNum.getText().toString();
+
+                // Calls method that sends messages
+                sendMsg(phoneNumber, myMsg);
             }
         });
 
 
+    }
+
+    private void sendMsg(String phoneNumber, String myMsg) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, myMsg, null, null);
     }
 }
