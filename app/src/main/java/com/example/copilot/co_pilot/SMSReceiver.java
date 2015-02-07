c064709aa8d44a1768b6ea08e8e438ad10960deb
Jacqueline M Ali <dragonperson@gmail.com>
2015-02-07 16:35:41 -0500

    Can You See My Messages?

--- app/src/main/java/com/example/copilot/co_pilot/SMSReceiver.java
+++ app/src/main/java/com/example/copilot/co_pilot/SMSReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 /**
  * Created by jacquelineali on 2/7/15.
@@ -16,5 +17,18 @@ public class SMSReceiver extends BroadcastReceiver {
         Bundle bundle = intent.getExtras();
         SmsMessage[] messages = null;
         String str = "";
+        if (bundle != null) {
+            Object pdus[] = (Object[]) bundle.get("pdus");
+            messages = new SmsMessage[pdus.length];
+            for(int i=0; i < messages.length; i++){
+                messages[i]=SmsMessage.createFromPdu((byte[])pdus[i]);
+                str += "Message from " + messages[i].getOriginatingAddress();
+                str += " :";
+                str += messages[i].getMessageBody().toString();
+                str += "\n";
+            }
+            // display the message
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+        }
     }
 }
