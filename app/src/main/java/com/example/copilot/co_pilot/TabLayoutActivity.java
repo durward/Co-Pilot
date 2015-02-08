42796d5dbe2d8530ba06bd704d08e9c0cc43d667
durward <durward3@cox.net>
2015-02-07 20:13:00 -0500

    Created Copilot view

---
+++ app/src/main/java/com/example/copilot/co_pilot/TabLayoutActivity.java
@@ -0,0 +1,11 @@
+package com.example.copilot.co_pilot;
+
+import android.app.TabActivity;
+
+/**
+ * Created by durwardbenham3 on 2/7/15.
+ */
+public class TabLayoutActivity extends TabActivity {
+
+
+}
